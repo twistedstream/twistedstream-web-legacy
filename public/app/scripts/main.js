2e5159dbe8203c173a153e1e285c3569d759b63a
Peter Stromquist <peter@twistedstream.com>
2015-02-03 00:31:15 -0800

    Build UI that allows a user to submit an answer

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,5 +1,61 @@
 (function () {
   'use strict';
 
-  console.log('Web app has started!');
+  $(function () {
+    console.log('Web app has started!!!');
+
+    $.getJSON('/api/challenge').done(function (data) {
+      // load and sytax highlight the challenge code
+      $('#challenge-code').text(data.code);
+      hljs.initHighlighting();
+
+      $('#resume').on('shown.bs.collapse', function () {
+        var answerTextarea = $('#answer');
+        answerTextarea.focus();
+
+        // run workflow
+        var runButton = $('#run');
+        runButton.click(function () {
+          // disable controls while checking answer
+          answerTextarea.prop('disabled', true);
+          runButton.prop('disabled', true);
+          var answerGroup = $('#answer-group');
+          var answerLabel = $('#answer-label');
+
+          $.ajax({
+            type: 'POST',
+            url: '/api/question',
+            data: JSON.stringify({ code: answerTextarea.val() }),
+            contentType: 'application/json'
+          }).fail(function (err) {
+            answerGroup.addClass('has-error');
+            // re-enable controls
+            answerTextarea.prop('disabled', false);
+            runButton.prop('disabled', false);
+
+            answerLabel.text(err.responseJSON.message);
+          }).done(function (data) {
+            answerGroup.removeClass('has-error');
+            answerLabel.text('');
+
+            $('#access-token').text(data.access_token);
+
+            // get personal resume formats and fill in other dynamic data
+            $.getJSON('/api/personal_resume/formats').done(function (data) {
+              $.each(data, function (i, format) {
+                var li = '<li>' + format.description + ': <code>' + format.contentType + '</code></li>';
+                $('#personal-resume-formats').append(li);
+              });
+
+              $('#personal-resume-resource').text(window.location + 'api/personal_resume');
+              $('#stackoverflow-resume-resource').text(window.location + 'api/stackoverflow_resume');
+
+              $('#token-block').show();
+              $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+            });
+          });
+        });
+      });
+    });
+  });
 })();
