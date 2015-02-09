42b20f7682c3d58688370c661ce816fba74b0a7e
Peter Stromquist <peter@twistedstream.com>
2015-02-09 08:06:17 -0600

    Remove untagged images on deploy

--- run-docker-prod.sh
+++ run-docker-prod.sh
@@ -14,5 +14,8 @@ if [[ $RUNNING_CONTAINERS ]]; then
   docker rm $RUNNING_CONTAINERS
 fi
 
+# remove any untagged images
+docker rmi $(docker images -f "dangling=true" -q)
+
 # run the container from the new image with the passed config data
 docker run -d -p 80:8080 -e "JWT_SECRET=$1" -e "SANDBOX_TIMEOUT=$2" -e "GOOGLE_DOCS_RESUME_BASE_EXPORT_URL=$3" -e "STACK_OVERFLOW_CAREERS_URL=$4" app
